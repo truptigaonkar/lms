c23df8661fd2b1dde809c0f8fafbc23c763f0a4c
truptigaonkar <trups11.trupti@gmail.com>
2018-04-08 17:46:44 +0200

    Admin-Librarian login functionality added successfully

---
+++ librarian/login.php
@@ -0,0 +1,97 @@
+<?php include"connection.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Library MGMT | Admin-Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="css/blue.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Library</b>MGMT</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg"><b>Admin-Librarian Login Form</b></p>
+
+    <form name="form1" action="" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" name="username" class="form-control" placeholder="Username" required=""/>
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" class="form-control" placeholder="Password" required=""/>
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat" name="submit1" value="Login">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+<?php
+    if(isset($_POST["submit1"]))
+    {
+        $count=0;
+        //$password = md5($_POST['password']);
+        $result=mysqli_query($link,"select * from librarian_registration where username='$_POST[username]' && password='$_POST[password]' ");
+        $count=mysqli_num_rows($result);
+        
+        if($count==0)
+        {
+            ?>
+                <div class="alert alert-warning col-lg-12 col-lg-push-0">
+                    <strong style="color:white">Invalid</strong> Username Or Password.
+                </div>
+            <?php
+        }
+        else
+        {
+            echo "login successfully";
+            header("Location:home.php");    
+        }
+    }
+?>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="js/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
+</body>
+</html>
+
--- student/login.php
+++ student/login.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Library MGMT | Log in</title>
+  <title>Library MGMT | User-Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -29,7 +29,7 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg"><b>Student Login Form</b></p>
+    <p class="login-box-msg"><b>User-Student Login Form</b></p>
 
     <form name="form1" action="" method="post">
       <div class="form-group has-feedback">
