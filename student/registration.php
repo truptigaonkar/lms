a29832b411704b8ece80c420677db82e16aa86ad
truptigaonkar <trups11.trupti@gmail.com>
2018-04-08 12:05:29 +0200

    Student Registration Done successfully

---
+++ student/registration.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Student Registration Form | LMS </title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/custom.min.css" rel="stylesheet">
+</head>
+
+<br>
+
+<div class="col-lg-12 text-center ">
+    <h1 style="font-family:Lucida Console">Library Management System</h1>
+</div>
+
+
+<body class="login" style="margin-top: -20px;">
+
+
+
+    <div class="login_wrapper">
+
+            <section class="login_content" style="margin-top: -40px;">
+                <form name="form1" action="" method="post">
+                    <h2>Student Registration Form</h2><br>
+
+                    <div>
+                        <input type="text" class="form-control" placeholder="FirstName" name="firstname" required=""/>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" placeholder="LastName" name="lastname" required=""/>
+                    </div>
+
+                    <div>
+                        <input type="text" class="form-control" placeholder="Username" name="username" required=""/>
+                    </div>
+                    <div>
+                        <input type="password" class="form-control" placeholder="Password" name="password" required=""/>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" placeholder="email" name="email" required=""/>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" placeholder="contact" name="contact" required=""/>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" placeholder="SEM" name="sem" required=""/>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" placeholder="Enrollment No" name="enrollmentno" required=""/>
+                    </div>
+                    <div class="col-lg-12  col-lg-push-3">
+                        <input class="btn btn-default submit " type="submit" name="submit1" value="Register">
+                    </div>
+
+                </form>
+            </section>
+    <?php
+    if(isset($_POST["submit1"]))
+    {
+        $link=mysqli_connect("localhost","root","");
+        mysqli_select_db($link,"lms");
+        $password = md5($_POST['password']); 
+        mysqli_query($link,"insert into student_registration values('','$_POST[firstname]','$_POST[lastname]','$_POST[username]','$password','$_POST[email]','$_POST[contact]','$_POST[sem]','$_POST[enrollmentno]')");      
+         //header("Location:login.php");   
+         ?>
+            <div class="alert alert-success col-lg-12 col-lg-push-0">
+                Registration successfully, You will get email when your account is approved
+            </div>
+        <?php
+    }
+    ?>
+
+    </div>
+
+
+
+
+</body>
+</html>
